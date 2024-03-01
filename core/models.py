8b08287f2576113bdf6f85d838c0f9fa37bd80b0
Zakaria <zakaria.aissaoui09@outlook.com>
2024-03-01 16:30:26 +0100

    6 commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profileimg = models.ImageField(upload_to='profile_images')
+    location = pass
