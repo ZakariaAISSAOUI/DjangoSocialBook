3ef45c11a188dde75fcdd9050528d534d7b2520d
Zakaria <zakaria.aissaoui09@outlook.com>
2024-03-01 20:23:08 +0100

    7 commit Signup

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture')
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm = request.POST['confirm']
+
+        if password == confirm:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Token")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "UserName Token")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # Log user in and redirect to settings page
+
+                # create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                user_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
+                user_profile.save()
+                return redirect('signup')
+
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
