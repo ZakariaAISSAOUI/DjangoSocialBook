0702059e45f9eca9d82303cedb1ab08dcd7dcd7f
Zakaria <zakaria.aissaoui09@outlook.com>
2024-03-12 22:33:51 +0100

    10 commit Account_Settings

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+
+
 # from django.http import HttpResponse
 
 
@@ -10,6 +12,36 @@ from .models import Profile
 @login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') is None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') is not None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('/settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signin(request):
@@ -49,12 +81,14 @@ def signup(request):
                 user.save()
 
                 # Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 user_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 user_profile.save()
-                return redirect('signin')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Password not matching')
