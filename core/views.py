7e28ce5898acf46821d3587d7cd9dfe19abcff1e
Zakaria <zakaria.aissaoui09@outlook.com>
2024-03-13 23:01:11 +0100

    11 commit Uploading Post *

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post
 
 
 # Create your views here.
@@ -16,7 +16,19 @@ def index(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<h1> Upload Post </h1>')
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
 
 
 @login_required(login_url='signin')
