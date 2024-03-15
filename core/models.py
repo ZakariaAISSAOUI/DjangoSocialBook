e7a287b2c307d215bb7f1436004a4eb1062321c0
Zakaria <zakaria.aissaoui09@outlook.com>
2024-03-15 14:54:17 +0100

    13 commit Like Post

--- core/models.py
+++ core/models.py
@@ -28,3 +28,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.user
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 # Create your views here.
@@ -31,6 +31,29 @@ def upload(request):
 
     else:
         return redirect('/')
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter is None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes += 1
+        post.save()
+        return redirect('/')
+
+    else:
+        like_filter.delete()
+        post.no_of_likes -= 1
+        post.save()
+        return redirect('/')
+
 
 
 @login_required(login_url='signin')
